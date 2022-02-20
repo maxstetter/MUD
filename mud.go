bab8e8a424aa59299d9fefdbc520a3622d8754c9
Polina Coulter <poolsrules@gmail.com>
2022-02-20 16:31:39 -0700

    readRoom() reads a single room's id, name and description. Stores and prints room object.

--- mud.go
+++ mud.go
@@ -2,15 +2,46 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"errors"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 ////GLOBAL////
 var Commands = make(map[string]func(string))
+var Zones = make(map[int]*Zone)
+var Rooms = make(map[int]*Room)
+var Directions = make(map[string]int)
+var player = Player{}
+var db *sql.DB
+
+type Zone struct {
+	ID    int
+	Name  string
+	Rooms []*Room
+}
+
+type Room struct {
+	ID          int
+	Zone        *Zone
+	Name        string
+	Description string
+	Exits       [6]Exit
+}
+
+type Exit struct {
+	To          *Room
+	Description string
+}
+
+type Player struct {
+	Room *Room
+}
 
 //END GLOBAL//
 
@@ -96,6 +127,7 @@ func initialize() {
 	addCommand("north", doNorth)
 	addCommand("east", doEast)
 	addCommand("west", doWest)
+	//up, down, say, tell, shout, pretty call?
 }
 
 func doCommand(command string) error {
@@ -140,7 +172,64 @@ func commandLoop() error {
 	return nil
 }
 
+//This function opens the database, reads a single and stoes the ID, Name and Descriptions fields in a Room object, prints this object out
+func readRooms(db *sql.DB) error {
+	//select id, zone_id, name, description from rooms where id = 3001;
+	rows, err := db.Query("SELECT id, name, description FROM rooms where ID = 3001")
+	if err != nil {
+		return fmt.Errorf("querying a room from the database: %v", err)
+	}
+
+	for rows.Next() {
+		var id int
+		var name, description string
+		if err := rows.Scan(&id, &name, &description); err != nil {
+			return fmt.Errorf("reading a room from the database: %v", err)
+		}
+		//var room = Room{id, Zones[zone_id], name, description, exits}
+		room := Room{ID: id, Name: name, Description: description}
+		fmt.Println(room)
+	}
+
+	return nil
+}
+
 func main() {
+	//	Directions["n"] = 0
+	//	Directions["e"] = 1
+	//	Directions["s"] = 2
+	//	Directions["w"] = 3
+	//	Directions["u"] = 4
+	//	Directions["d"] = 5
+
+	// the path to the database--this could be an absolute path
+	path := "world.db"
+	options := "?" + "_busy_timeout=10000" +
+		"&" + "_foreign_keys=ON"
+	db, err := sql.Open("sqlite3", path+options)
+	if err != nil {
+		log.Fatalf("opening database: %v", err)
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatalf("begin room read transaction: %v", err)
+	}
+	stmt, err := tx.Prepare(`SELECT * FROM rooms`)
+	if err != nil {
+		log.Fatalf("prepare room read transaction: %v", err)
+	}
+	defer stmt.Close()
+
+	e := readRooms(db)
+	if e != nil {
+		log.Fatalf("prepare room read transaction: %v", e)
+		fmt.Println("error?")
+		tx.Rollback()
+	} else {
+		tx.Commit()
+	}
 	fmt.Println("WELCOME TO THE DUNGEON")
 	fmt.Println("Enter: ") //Ask for input here?
 	// use time and origin file for log prefixes
